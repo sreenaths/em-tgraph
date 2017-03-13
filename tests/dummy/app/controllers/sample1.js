6c49e15ba797c295ab05906ef5e388e443365e49
Sreenath Somarajapuram <sree@apache.org>
2017-03-13 23:39:49 +0530

    Adding an empty vertexProperties into sample

--- tests/dummy/app/controllers/sample1.js
+++ tests/dummy/app/controllers/sample1.js
@@ -157,6 +157,7 @@ export default Ember.Controller.extend({
         "edgeDestinationClass": "org.apache.tez.runtime.library.input.OrderedGroupedKVInput"
       }
     ]
-  }
+  },
+  vertexProperties: []
 
 });
